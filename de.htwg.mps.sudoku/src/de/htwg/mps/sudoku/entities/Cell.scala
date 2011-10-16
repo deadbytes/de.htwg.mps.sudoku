819e7f2d37f1464e854ad3daa3f67492afb1396e
Marko Boger <marko.boger@gmail.com>
2011-10-16 19:53:59 +0200

    Only one class (Cell) with simple methods, no control structures

---
+++ de.htwg.mps.sudoku/src/de/htwg/mps/sudoku/entities/Cell.scala
@@ -0,0 +1,21 @@
+package de.htwg.sudoku.entities
+
+class Cell(row: Int, column: Int) {
+  var value = 0
+  var given = false
+  var showCandidates = false
+
+  def :=(v: Int) = value = v
+  def ==(v: Int) = if (value == v) true else false
+  def isSet = value != 0
+  def x = row
+  def y = column
+  def mkString = "(" + row + ", " + column + ") = " + value
+  override def toString = value.toString.replace('0', ' ')
+
+}
+
+
+
+
+
